52b10deb6b9122cf511401220ab8f16ac524a2f5
lw_mengo <867740815@qq.com>
2018-01-22 17:22:22 +0800

    homepager complete

--- app/src/main/java/example/study/com/myapp/module/MainActivity.java
+++ app/src/main/java/example/study/com/myapp/module/MainActivity.java
@@ -1,12 +1,15 @@
 package example.study.com.myapp.module;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import example.study.com.myapp.R;
 import example.study.com.myapp.base.RxBaseActivity;
 import example.study.com.myapp.fragment.HomePage;
 
 public class MainActivity extends RxBaseActivity {
+    private static final String TAG="MainActivity";
+
     private HomePage mHomePage;
 
 
@@ -28,6 +31,7 @@ public class MainActivity extends RxBaseActivity {
 
     private void initFragment() {
         mHomePage = HomePage.newInstance();
+        Log.d(TAG, "initFragment: +"+"222");
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.container, mHomePage)
